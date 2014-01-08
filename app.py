6e460f35284e0eec2e8a2753d07496787f96929f
Jonathan Wight <jwight@mac.com>
2014-01-07 16:26:54 -0800

    Save pre-processed json.

--- app.py
+++ app.py
@@ -9,34 +9,7 @@ import os
 
 app = Flask(__name__)
 
-info = json.load(file('Info.json'))
-all_types = info['UTExportedTypeDeclarations'] + info['UTImportedTypeDeclarations']
-types_by_identifier = { t['UTTypeIdentifier']: t for t in all_types }
-del all_types
-
-def listify(o):
-    if isinstance(o, basestring):
-        return [o]
-    else:
-        return o
-
-for t in types_by_identifier.values():
-    if 'UTTypeConformsTo' in t:
-        t['UTTypeConformsTo'] = listify(t['UTTypeConformsTo'])
-        if 'UTTypeTagSpecification' in t:
-            tags = t['UTTypeTagSpecification']
-            if 'public.filename-extension' in tags:
-                tags['public.filename-extension'] = listify(tags['public.filename-extension'])
-            if 'public.mime-type' in tags:
-                tags['public.mime-type'] = listify(tags['public.mime-type'])
-
-        for conforming_t in t['UTTypeConformsTo']:
-            if conforming_t not in types_by_identifier:
-                types_by_identifier[conforming_t] = { 'UTTypeIdentifier': conforming_t}
-
-            if 'UTTypeConformedBy' not in types_by_identifier[conforming_t]:
-                types_by_identifier[conforming_t]['UTTypeConformedBy'] = []
-            types_by_identifier[conforming_t]['UTTypeConformedBy'].append(t['UTTypeIdentifier'])
+types_by_identifier = json.load(file('processed.json'))
 
 @app.route("/")
 @app.route("/roots")
